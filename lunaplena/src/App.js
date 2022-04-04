2cd439f7bc524bdb9354531a69b2f0a044514534
juanpdoss <juanpdoss@gmail.com>
2022-04-04 00:29:58 -0300

    divino los react hooks

--- lunaplena/src/App.js
+++ lunaplena/src/App.js
@@ -2,31 +2,36 @@
 // using images with a folder and import word
 import lofiFondo from "./assets/lofiFondo.jpg";
 import "./App.css";
+import { useState } from "react";
 
-// using react hook called 
+// using react hook called
 // *react hooks are just functions who do good shi*t for us
 // all react hooks starts with 'use' word
 
 // again, a react component is just a function
 function App() {
-  let name = "Lofi";
+  const name = "Lofi";
   const buttonMessage = "Cambiar fondo de color";
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+  // array destructuring
+  // the first value is the actual value returned by useState function
+  // the second value is a function which would use to change the state value
+  const [color, setColor] = useState(randomColor);
 
   // handler functions
   const changeBackgroundColor = () => {
     // we need to use the hook status to make react re-render the DOM, applying changes done in runtime
-
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    console.info("evento handleado, color random:", randomColor);
-    document.getElementById("root").style.backgroundColor = randomColor;
+    setColor(Math.floor(Math.random() * 16777215).toString(16));
   };
 
   // we return it, always
   return (
-    <div className="App" id="parent">
+    <div className="App" id="parent" style={{ backgroundColor: randomColor }}>
       <img src={lofiFondo} className="App-img" alt="logo" />
       <h1>Soy un fondo lofi {name}, jeje .</h1>
       <button onClick={changeBackgroundColor}>{buttonMessage}</button>
+      <p>Color random actual {randomColor}</p>
     </div>
   );
 }

89bdb9862393ae337bcbc51e5d3b1dba30e9ec20
juanpdoss <juanpdoss@gmail.com>
2022-04-20 16:49:05 -0300

    agrego un return alternativo en componente RecipeList en caso de que el array recibido tenga length = 0

--- cooking-ninja/src/components/RecipeList.js
+++ cooking-ninja/src/components/RecipeList.js
@@ -7,6 +7,13 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 export default function RecipeList({ recipes }) {
+  
+  if(recipes.length === 0){
+    return <div className="error">No recipes match the term ...</div>;
+  }
+  
+  
+  
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => {
